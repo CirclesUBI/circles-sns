73f81be3b0b001b452c92fe01a1ed67347be6b6e
ed murphy <edzillion@gmail.com>
2018-12-01 16:42:27 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var push = require('./push.js')
+var bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+var app = express()
+
+// Body parser extracts POST data from incoming
+// requests for our use
+app.use(bodyParser())
+
+// Path used to register for push notifications
+// push.register is the handler function
+app.post('/register', push.register)
+
+app.listen(80, function (error) {
+  if (error) {
+    // Something unexpected happened
+    throw error
+  }
+
+  console.log('Listening on 127.0.0.1:80')
+})
---
+++ push.js
@@ -0,0 +1,41 @@
+// We need to use the sns-mobile module
+var SNS = require('sns-mobile')
+require('dotenv').config()
+
+var myApp = new SNS({
+  platform: SNS.SUPPORTED_PLATFORMS.ANDROID,
+  region: 'eu-central-1',
+  apiVersion: '2010-03-31',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  platformApplicationArn: process.env.ANDROID_ARN
+})
+
+// Handle user added events
+myApp.on('userAdded', function (endpointArn, deviceId) {
+  console.log('\nSuccessfully added device with deviceId: ' + deviceId + '.\nEndpointArn for user is: ' + endpointArn)
+  // Maybe do some other stuff...
+})
+
+exports.register = function (req, res) {
+  console.log(req)
+  var deviceId = req.body['deviceId']
+
+  console.log('\nRegistering user with deviceId: ' + deviceId)
+
+  // Add the user to SNS
+  myApp.addUser(deviceId, null, function (err, endpointArn) {
+    // SNS returned an error
+    if (err) {
+      console.log(err)
+      return res.status(500).json({
+        status: 'not ok'
+      })
+    }
+
+    // Tell the user everything's ok
+    res.status(200).json({
+      status: 'ok'
+    })
+  })
+}
